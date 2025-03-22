ae18503f9dec229693fe6dd10220cdfe12581ff6
Kai-Thanos <mengkemom@gmail.com>
2025-03-22 12:00:29 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -28,6 +28,9 @@ window.addEventListener("scroll", function(){
 //     event.currentTarget.classList.add("isActive")
 // }
 
+
+
+// ===============> Side-Menu <=====================
 const showMenu = ()=>{
     let sideMenu = document.getElementById("isShow");
 
@@ -39,3 +42,24 @@ const isShowDraw = ()=>{
 
     popDraw.classList.toggle("isPop-draw")
 }
+
+// ================> Contact <================
+const form = document.querySelector('form')
+function sendEmail(){
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "mommengke01@gmail.com",
+        Password : "E7B3EB73C4950F5A21851C49315D696C33FA",
+        To : 'mommengke01@gmail.com',
+        From : "mommengke01@gmail.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+
+    sendEmail();
+})
